3b72adb8a370fb2b43960bdef97c8f9714ecccda
IDislikeName <yilaisun@yahoo.com>
2023-11-01 14:57:38 -0400

    stuff

--- Assets/Scripts/DialogueContent.cs
+++ Assets/Scripts/DialogueContent.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class DialogueContent
 {
     [SerializeField] private string speaker;
-    [SerializeField] [TextArea] private string  dialgoueText;
+    [SerializeField] [TextArea(15,20)] private string  dialgoueText;
     [SerializeField] private int spriteIndex;
 
     public string Speaker => speaker;
